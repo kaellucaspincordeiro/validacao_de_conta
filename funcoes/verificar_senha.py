08fe2585267dcaf73daaa27023f1380fb72e8268
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-06-30 18:19:07 -0300

    Ajuste: Atualizando a função verificar_senha

--- funcoes/verificar_senha.py
+++ funcoes/verificar_senha.py
@@ -1,13 +1,12 @@
 def verificar_senha(senha):
 
-    verificacao_senha = (len(senha) >= 10
-                        and senha.upper()
-                        and senha.lower()
-                        and senha.isascii()
-                        )
+    maiuscula = any(s.isupper() for s in senha)
+    minuscula = any(s.islower() for s in senha)
+    caractere_especial = any(not s.isalnum() for s in senha)
 
-    if verificacao_senha:
-        return "Senha cadastrada"
-    else:
-        return "Senha inválida"
+    return (len(senha) >= 10
+            and maiuscula
+            and minuscula
+            and caractere_especial
+           )
 
