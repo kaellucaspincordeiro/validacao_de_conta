142f383cb3aaf7552fdd58c6eac818a42ab4e200
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-06-30 18:29:09 -0300

    Ajustes do fluxo principal de cadastro e login

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from classe.Senha import Senha
 from funcoes.verificar_email import verificar_email
 from funcoes.verificar_senha import verificar_senha
 from getpass import getpass
+from colorama import Fore, Back, Style, init
+
+init()
 
 email_guardado = ""
 senha_guardada = ""
+bloqueio = False
 
 while True:
 
-    print("""
+    print(Fore.WHITE + """
 1) Cadastrar conta
 2) Entrar conta
 3) Encerrar o programa
@@ -30,33 +34,41 @@ while True:
 
             email_guardado = email.email
             senha_guardada = senha.senha
-
-            print(cadastrar_email)
-            print(cadastrar_senha)
+    
+            if cadastrar_email and cadastrar_senha:
+                print(Fore.LIGHTGREEN_EX + "Conta cadastrada com sucesso")
+            else:
+                print(Fore.RED + Style.NORMAL + "Conta inválida")
 
         case "2":
+
+            if email_guardado == "" and senha_guardada == "":
+                print(Fore.WHITE + "Nenhuma conta criada. Cadastre a sua conta primeiro.")
+                continue
 
             tentativa = 3
 
             while tentativa > 0:
 
-                email_cadastrado = input("Digite seu e-mail: ")
-                senha_cadastrada = getpass("Digite sua senha: ")
+                email_cadastrado = input(Fore.WHITE + "Digite seu e-mail: ")
+                senha_cadastrada = getpass(Fore.WHITE + "Digite sua senha: ")
 
                 if email_cadastrado == email_guardado and senha_cadastrada == senha_guardada:
 
-                    print("Login realizado com sucesso!")
+                    print(Fore.LIGHTGREEN_EX + "Login realizado com sucesso!")
                     break
 
                 else:
                     tentativa -= 1
+                    print(Fore.RED + Style.NORMAL + f"Email ou senha incorreta. Você tem {tentativa} tentativas!")
 
             if tentativa == 0:
-                print("Sua conta foi bloqueada após 3 tentativas incorretas.")
-                break   
+                print(Fore.RED + Style.NORMAL + "Sua conta foi bloqueada após 3 tentativas incorretas.")
+                bloqueio = True   
+                break
 
         case "3":
-            print("Encerrando o sistema.")   
+            print(Fore.WHITE + "Encerrando o sistema.")   
             break
 
 
